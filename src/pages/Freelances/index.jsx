ffaa157bb17b528f73cb4256f9385483473cf3ee
Moustapha <88998519+HayasMoustapha@users.noreply.github.com>
2025-10-15 08:48:05 +0100

    Installation of PropTypes and typing of the Card component's props

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,7 +1,37 @@
+import Card from "../../components/Card";
+import DefaultPicture from "../../assets/default-picture.png"
+
+
+const freelanceProfiles = [
+  {
+    name: 'Jane Doe',
+    jobTitle: 'Devops',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'John Doe',
+    jobTitle: 'Développeur frontend',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'Jeanne Biche',
+    jobTitle: 'Développeur backend',
+    picture: DefaultPicture,
+  },
+]
+
 const Freelances = () => {
   return (
     <div>
       <h1>This is the Freelances page</h1>
+      {freelanceProfiles.map((profile, index) => (
+        <Card
+          key={`${profile.name}-${index}`}
+          label={profile.jobTitle}
+          picture={profile.picture}
+          title={profile.name}
+        />
+      ))}
     </div>
   )
 }
