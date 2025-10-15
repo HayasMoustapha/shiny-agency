8d6dcb35584b7d3c70da645cd468f1fa5ba33ee9
Moustapha <88998519+HayasMoustapha@users.noreply.github.com>
2025-10-15 08:58:43 +0100

    Update Card Component's props

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,4 +1,5 @@
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
+import DefaultPicture from "../../assets/profile.png"
 
 const Card=({ label, title, picture }) =>{
   return (
@@ -11,9 +12,16 @@ const Card=({ label, title, picture }) =>{
 }
 
 Card.propTypes = {
-  label: PropTypes.string,
-  title: PropTypes.string,
-  picture: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
 }
+
+Card.defaultProps = {
+  label: "'Freelance'",
+  title: "",
+  picture: DefaultPicture,
+}
+
 
 export default Card
